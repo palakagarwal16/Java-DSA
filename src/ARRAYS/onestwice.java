6e2135b7cac77fcf184d562cb5ffb19b2fe0e35b
palakagarwal16 <palakagarwal1635@gmail.com>
2025-08-04 23:32:29 +0530

    onestwivce

--- src/ARRAYS/onestwice.java
+++ src/ARRAYS/onestwice.java
@@ -1,2 +1,12 @@
-package ARRAYS;public class onestwice {
+package ARRAYS;
+
+public class onestwice {
+    public int singleNumber(int[] nums) {
+        int XOR = 0 ;
+        for (int i = 0 ; i< nums.length; i++){
+            XOR = XOR^nums[i];
+        }
+        return XOR ;
+    }
 }
+
