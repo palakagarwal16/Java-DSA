900a2af79a3f7a81a513ece54c4a8c5ef69d7cf1
palakagarwal16 <palakagarwal1635@gmail.com>
2025-08-09 15:43:58 +0530

    input file added

---
+++ array/Input.java
@@ -0,0 +1,20 @@
+package array;
+
+public class Input {
+    public static void main(String[] args) {
+        System.out.println( "Linear Search of 234 ");
+    int arr [] = {2 , 4 ,56 ,7 , 56 , 34 , 77 , 234 , 56 } ;
+    boolean flag = false ;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 234 ){
+                System.out.println( "Element found on index " + i );
+                flag = true;
+                break;
+            } }
+            if(flag == false){
+                System.out.println( "Not found ");
+            }
+
+    }
+}  
+
